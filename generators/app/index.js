8ec9d5ce70f1e5fa95733fe816da5da90c51d4e6
Best <potikornth@gmail.com>
2021-10-04 16:15:47 +0700

    first init

---
+++ generators/app/index.js
@@ -0,0 +1,68 @@
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+    this.argument("appname", { type: String, required: false });
+  }
+
+  // Async await
+  async prompting() {
+    this.answers = await this.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Your project name",
+        default: this.appname,
+        store: true,
+      },
+      {
+        type: "list",
+        name: "templateType",
+        message: "Select the template wanted:",
+        choices: [
+          "Front-End React",
+          "Node API builder",
+          "Fullstack Application",
+        ],
+      },
+    ]);
+  }
+
+  install() {
+    this.npmInstall();
+  }
+
+  writing() {
+    if (this.answers.templateType === "Front-End React") {
+      this._writingReactTemplate();
+    } else if (this.answers.templateType === "Node API builder") {
+      this._writingApiTemplate();
+    } else {
+      this._writingReactTemplate();
+      this._writingApiTemplate();
+    }
+  }
+
+  _writingReactTemplate() {
+    this.fs.copy(
+      this.templateType("frontend"),
+      this.destinationPath("frontend")
+    );
+    this.fs.copyTpl(
+      this.templatePath("frontend/public/index.html"),
+      this.destinationPath("frontend/public/index.html"),
+      { title: this.answers.name } // Embedded JavaScript templating.
+    );
+  }
+
+  _writingApiTemplate() {
+    this.fs.copy(this.templatePath("api"), this.destinationPath("api"));
+  }
+
+  end() {
+    this.log(chalk.green("------------"));
+    this.log(chalk.magenta("***---***"));
+    this.log(chalk.blue("Jobs is Done!"));
+    this.log(chalk.green("------------"));
+    this.log(chalk.magenta("***---***"));
+  }
+};
